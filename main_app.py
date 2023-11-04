a332cb566adcba27eef2c2500f429f41c9c8ac8c
Terry Su <moonsolol@yahoo.com>
2023-11-04 13:35:46 -0400

    dataset 2
    replaced sea animals dataset with boat dataset

--- main_app.py
+++ main_app.py
@@ -1,8 +1,13 @@
 import streamlit as st
 
-import tensorflow as tf
 import pandas as pd
 import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from keras.optimizers import Adam
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, BatchNormalization
+from keras.preprocessing.image import ImageDataGenerator
 
 def main():
     st.title('HackNJIT 2024 Project')
